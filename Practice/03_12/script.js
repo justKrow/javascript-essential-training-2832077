a8cd3c29f584a32c42b1adb673fa861429264d94
justKrow <krowxen2214@gmail.com>
2025-01-28 08:47:25 +0000

    practice class and object build

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,34 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class Laptop {
+  constructor(model, brand, price, screenSize, processor, memory, isLidOpen) {
+    this.model = model;
+    this.brand = brand;
+    this.price = price;
+    this.screenSize = screenSize;
+    this.processor = processor;
+    this.memory = memory;
+    this.isLidOpen = isLidOpen;
+  }
+  toggleLid() {
+    this.isLidOpen = !this.isLidOpen;
+  }
+}
+
+const acer = new Laptop(
+  "Aspire 5",
+  "Acer",
+  450,
+  "15.6 inches",
+  "Intel Core i5-1035G1",
+  "8GB DDR4",
+  false
+);
+
+console.log(acer.isLidOpen);
+console.log(acer.toggleLid());
+console.log(acer.isLidOpen);
+
+export default Laptop;
