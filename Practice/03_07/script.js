cf015a670a689360464cd182b3dda1eac981cdab
justKrow <krowxen2214@gmail.com>
2025-01-28 06:56:07 +0000

    practice object build

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,21 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const cupboard = {
+  doorNum: 2,
+  drawerNum: 5,
+  height: 48,
+  width: 36,
+  depth: 12,
+  hasClothesHanger: false,
+  changeClothesHanger: function (clothesHangerStatus) {
+    this.hasClothesHanger = clothesHangerStatus;
+  },
+  contentsInside: {
+    booksNum: 3,
+    shirtsNum: 15,
+    pantsNum: 10,
+    socksNum: 9,
+  },
+};
