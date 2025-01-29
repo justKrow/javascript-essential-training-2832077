8b00bfbfb2557bc7e79372c6ed7a4a500b60e1bc
justKrow <krowxen2214@gmail.com>
2025-01-29 21:31:48 +0000

    modify class attributes and style

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,17 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+console.log(document.querySelector("h1").classList.add("hidden"));
+console.log(document.querySelector("h1").classList.add("test"));
+console.log(document.querySelector("h1").classList.remove("hidden"));
+console.log(
+  document.querySelector(".backpack__features").setAttribute("alt", "test")
+);
+console.log(document.querySelector(".backpack__features").getAttribute("alt"));
+console.log(
+  document.querySelector(".backpack__features").setAttribute("alt", "pass")
+);
+console.log(
+  (document.querySelector(".backpack__features").style.color = "blue")
+);
